4f6f41c71ccd66af641dabf738c9306226b2a413
Yohanan-Negash <yoniget066@gmail.com>
2022-05-25 22:31:03 -0400

    Define routes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/database');
+const auth = require('./middleware/auth');
 
 //Connect to DB
 connectDB();
@@ -10,6 +11,13 @@ app.use(express.json());
 
 //Bring in env
 const port = process.env.PORT || 3001;
+
+//Define routes
+app.use('/api/login', require('./routes/login'));
+app.use('/api/register', require('./routes/register'));
+app.post('/welcome', auth, (req, res) => {
+  res.status(200).send('Welcome');
+});
 
 //Create server that listens to PORT
 app.listen(port, () => console.log(`Server is running on PORT:${port}`));
